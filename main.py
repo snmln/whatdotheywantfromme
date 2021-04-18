5f99e8c52219c089a19d8bd0c96b1766fd0d8808
snmln <designbymalone@gmail.com>
2021-04-17 20:25:23 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.indeed.com/jobs?q=developer&l=Columbus%2C+OH')
+
+
+# https://www.indeed.com/
+# Need to fogure out how to take input informaiton
+# from cields and place as a new indeed.com search
+
+# developer columbus ohio
